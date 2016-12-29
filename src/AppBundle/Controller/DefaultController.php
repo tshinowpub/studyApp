9ff430f751369045d31598682e52e0a36c671611
shinohara-surface <shinohara@n-di.co.jp>
2016-12-29 16:09:43 +0900

    デフォルトのテンプレートを削除し、空のテンプレートを作成した

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -14,8 +14,13 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        /**return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);**/
+
+        return $this->render('index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
+
     }
 }
