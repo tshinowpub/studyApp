10d14b48dd0d6b67d13ed54cf250a526582c895b
shinohara-surface <shinohara@n-di.co.jp>
2017-01-26 23:55:18 +0900

    ぐるなびのAPIのサンプル取得を追加した

--- src/StudyApp/CoreBundle/Controller/Admin/Store/StoreController.php
+++ src/StudyApp/CoreBundle/Controller/Admin/Store/StoreController.php
@@ -3,9 +3,15 @@
 namespace StudyApp\CoreBundle\Controller\Admin\Store;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 
+use GuzzleHttp\Client;
+
 class StoreController {
+
+    const BASE_URL = 'https://api.gnavi.co.jp/RestSearchAPI/20150630/';
 
     /**
      * @Route("/admin/store")
@@ -25,6 +31,38 @@ class StoreController {
         return new Response(
             '<html><body>search</body></html>'
         );
+    }
+
+    /**
+     * @Route("/admin/store/api")
+     * @Template("StudyAppCoreBundle:Admin/Store:api.html.twig")
+     */
+    public function apiAction()
+    {
+        $client = new Client();
+
+        $response = $client->request('GET', self::BASE_URL, [
+            'verify' => false,
+            'query' => [
+                'keyid' => '',
+                'format' => 'json',
+            ]
+        ]);
+
+        $stores = [];
+        if($response->getStatusCode() == '200') {
+            $json = $response->getBody();
+            $stores = $this->decodeStoreDataByJson($json);
+        }
+
+        return [];
+    }
+
+    private function decodeStoreDataByJson($json)
+    {
+        $stores = json_decode($json);
+
+        return $stores;
     }
 
 }
